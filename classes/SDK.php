b12ac6ad9efdc4354aac955e88e49ab272060eb7
David Kelly <david@give.it>
2013-10-15 18:11:12 +0200

    add function to test API keys

--- classes/SDK.php
+++ classes/SDK.php
@@ -15,7 +15,7 @@ namespace GiveIt;
 
 class SDK extends SDK\Base
 {
-    const   VERSION          = '1.1.3';
+    const   VERSION          = '1.1.4';
 
     public  $dataKey         = null;
     public  $publicKey       = null;
@@ -187,6 +187,41 @@ class SDK extends SDK\Base
         }
 
         return $this->payments;
+    }
+
+    public function verifyKeys()
+    {
+        $client         = SDK\Client::getInstance();
+        $authenticated  = $client->authenticate();
+
+        if (! $authenticated) {
+            $this->addError("unable to log in with private key");
+            return false;
+        }
+
+        $result = $client->sendGET('/retailers/me');
+
+       if (isset($result->errors)) {
+            foreach ($result->errors as $error) {
+                $this->addError($error);
+            }
+            return false;
+        }
+
+        // at this point we can assume that the private key is okay, verify the other two
+
+        if ($result->public_api_key != $this->publicKey) {
+            $this->addError("incorrect public key");
+            return false;
+        }
+
+        if ($result->data_key != $this->dataKey) {
+            $this->addError("incorrect data key");
+            return false;
+        }
+
+        return true;
+
     }
 }
 
